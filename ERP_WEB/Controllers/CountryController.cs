e1f9186f0b63a4d75904fc504e91e55469e7ef39
Ronak Dhamsaniya <dhamsaniyaronak@gmail.com>
2022-07-08 02:15:19 +0530

    1.Add Location Master Degine

---
+++ ERP_WEB/Controllers/CountryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ERP_WEB.Controllers
+{
+    public class CountryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
